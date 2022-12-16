350513857091a88d6db8344cc5e5cb257d29316b
MorPfa <moritzjohannes.pfa1@seattlecolleges.edu>
2022-12-15 17:16:07 -0800

    .

--- it161/includes/multiple.php
+++ it161/includes/multiple.php
@@ -22,52 +22,52 @@
 <form action="<?php echo basename($_SERVER['PHP_SELF']); ?>" method="post">
 <div>
     <label>
-        Name:<br /><input type="text" name="Name" required="required" placeholder="Full Name (required)" title="Name is required" tabindex="10" size="44" autofocus />
+        Name:<br /><input type="text" name="Name" required="required" placeholder="Vollstandiger Name" title="Name is required" tabindex="10" size="44" autofocus />
     </label>
 </div>
 <div>	
     <label>
-        Email:<br /><input type="email" name="Email" required="required" placeholder="Email (required)" title="A valid email is required" tabindex="20" size="44" />
+        Email:<br /><input type="email" name="Email" required="required" placeholder="Email " title="A valid email is required" tabindex="20" size="44" />
     </label>
 </div>
 <!-- below change the HTML to your form elements - only 'Name' & 'Email' (above) are significant -->
 <div>	
     <label>
-        How Did You Hear About Us?:<br />
+        Wie haben sie von uns gehort?:<br />
         <select name="How_Did_You_Hear_About_Us?" required="required" title="How You Heard is required" tabindex="30">
-            <option value="">Choose How You Heard</option>
-            <option value="Phone">Phone</option>
-            <option value="Web">Web</option>
-            <option value="Magazine">Magazine</option>
-            <option value="A Friend">A Friend</option>
-            <option value="Other">Other</option>
+            <option value="">Optionen</option>
+            <option value="Phone">Telefon</option>
+            <option value="Web">Internet</option>
+            <option value="Magazine">Magazin</option>
+            <option value="A Friend">Freunde</option>
+            <option value="Other">Andere</option>
         </select>
     </label>
 </div>
 
 <div>	
     <fieldset>
-        <legend>What Services Are You Interested In?</legend>
-        <input type="checkbox" name="Interested_In[]" value="New Website" tabindex="40" /> New Website <br />
-        <input type="checkbox" name="Interested_In[]" value="Website Redesign" /> Website Redesign <br />
+        <legend>Woran sind sie interessiert?</legend>
+        <input type="checkbox" name="Interested_In[]" value="Local tour" tabindex="40" /> Regionale Tour <br />
+        <input type="checkbox" name="Interested_In[]" value="Neighboring country tour" /> Tour in eines der Nachbarlander <br />
         <input type="checkbox" name="Interested_In[]" value="Special Application" /> Special Application <br />
-        <input type="checkbox" name="Interested_In[]" value="Lollipops" /> Complimentary Lollipops <br />
-        <input type="checkbox" name="Interested_In[]" value="Other" /> Other <br />
+        <input type="checkbox" name="Interested_In[]" value="Accessiblity" /> Behinderungen/ Barrierefreiheit <br />
+        <input type="checkbox" name="Interested_In[]" value="Other" /> Etwas anderes <br />
     </fieldset>
 </div>
 
     <div>	
     <fieldset>
-        <legend>Would you like to join our mailing list?</legend>
+        <legend>Wurden sie gerne unseren Newsletter abonnieren?</legend>
         <input type="radio" name="Join_Mailing_List?" value="Yes" 
         required="required" title="Mailing list is required" tabindex="50"  
-        /> Yes <br />
-        <input type="radio" name="Join_Mailing_List?" value="No" /> No <br />
+        /> Ja <br />
+        <input type="radio" name="Join_Mailing_List?" value="No" /> Nein <br />
     </fieldset>
 </div>
 <div>	
     <label>
-        Comments:<br /><textarea name="Comments" cols="36" rows="4" placeholder="Your comments are important to us!" tabindex="60"></textarea>
+        Kommentare:<br /><textarea name="Comments" cols="36" rows="4" placeholder="Ihre Kommentare sind uns wichtig!" tabindex="60"></textarea>
     </label>
 </div>	
 <div class="g-recaptcha" data-sitekey="<?=$siteKey;?>"></div>
